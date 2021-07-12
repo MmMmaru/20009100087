f56cc4dca09864f002be7a3f7d798626685be34c
MmMmaru <73730571+MmMmaru@users.noreply.github.com>
2021-07-12 20:02:02 +0800

    Create led.c

---
+++ lessons2/led.c
@@ -0,0 +1,21 @@
+int i=13;
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+void loop()
+{
+  for(i=13;i>=7;i--)
+  {
+  	digitalWrite(i, HIGH);
+  	delay(1000); 
+  	digitalWrite(i, LOW);
+  	delay(1000); 
+  }
+}
